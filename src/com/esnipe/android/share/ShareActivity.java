c4c44942143c0bb5d26d52fed9c600578b759561
Denis Nelubin <dnelubin@gmail.com>
2012-09-23 01:38:47 +0700

    Implemented handling of the Intent.

--- src/com/esnipe/android/share/ShareActivity.java
+++ src/com/esnipe/android/share/ShareActivity.java
@@ -1,7 +1,10 @@
 package com.esnipe.android.share;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class ShareActivity extends Activity {
     /**
@@ -10,6 +13,28 @@ public class ShareActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        Intent intent = getIntent();
+        if (intent == null) {
+            exit();
+            return;
+        }
+
+        logIntent(intent);
     }
+
+    void logIntent(Intent intent) {
+        Log.d(Tag.TAG, "action: " + intent.getAction());
+        Log.d(Tag.TAG, "type: " + intent.getType());
+        Log.d(Tag.TAG, "data: " + intent.getDataString());
+        Log.d(Tag.TAG, "subj: " + intent.getStringExtra(Intent.EXTRA_SUBJECT));
+        Log.d(Tag.TAG, "text: " + intent.getCharSequenceExtra(Intent.EXTRA_TEXT));
+    }
+
+    void exit() {
+        Toast.makeText(this, R.string.cannot_open, Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+
 }
---
+++ src/com/esnipe/android/share/Tag.java
@@ -0,0 +1,11 @@
+package com.esnipe.android.share;
+
+public class Tag {
+
+    public static final String TAG = Tag.class.getPackage().getName();
+
+    private Tag() {
+        //avoid instantiation
+    }
+
+}
